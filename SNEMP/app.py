21e4ce0273005e64447fd3f4ea02d3fd8cedda2e
Bruno Ferrari Senhora <brunoferrarisenhora@gmail.com>
2022-06-27 16:22:06 -0300

    Implementação do Front-end (02)
    Melhoria na função do botao submit.

--- SNEMP/app.py
+++ SNEMP/app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import csv
 import os
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
-    if request.method == "POST":
-        if request.files:
-            planilha = request.files["csv"]
-            print(planilha)
-            return redirect(request.url)
-    
     return render_template("snemp.html")
+
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    if request.method == "POST":
+        f = request.form["csvfile"]
+        data = []
+        with open(f) as file:
+            csvfile = csv.reader(file)
+            for row in csvfile:
+                data.append(row)
+        data = pd.DataFrame(data)
+        return render_template("data.html", data = data)
+
+
 
 if __name__ ==  "__main__":
     app.run(debug=True)
