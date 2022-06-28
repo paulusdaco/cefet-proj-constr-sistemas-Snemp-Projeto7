b13b4e53231dd47d9465f3f620d64a0ac0b6ca79
Bruno Ferrari Senhora <brunoferrarisenhora@gmail.com>
2022-06-28 10:29:16 -0300

    Correção de Layout
    Modificado o layout do database na exibição da tela. Implementado uma conexão mais fiel com o db.

--- SNEMP/app.py
+++ SNEMP/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import pandas as pd
 import os
@@ -31,6 +31,15 @@ def index():
             con.close
             return render_template("snemp.html", data=data)
 
+    if request.method == "GET":
+        con = sqlite3.connect("MyData.db")
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("select * from data")
+        data = cur.fetchall()
+        con.close
+        return render_template("snemp.html", data=data)
+
     return render_template("snemp.html")
 
 @app.route('/bd/<string:id>')
@@ -45,13 +54,19 @@ def bd(id):
         data = pd.read_csv(path)
         data.to_html("output.html")
     con.close()
-    return render_template("bd.html", data=data.to_html(index=False, classes="table table-bordered text-white".replace('<th>', '<th style="text-align:center">')))
+    return render_template("bd.html", data=data.to_html(index=False, classes="table table-bordered text-white center").replace('<th>','<th class="align-middle" style="text-align:center">'))
 
+@app.route('/delete_record/<string:id>')
+def delete_record(id):
+    try:
+        con = sqlite3.connect("MyData.db")
+        cur = con.cursor()
+        cur.execute("delete from data where pid=?",(id))
+        con.commit()
+    finally:
+        return redirect(url_for("index"))
+        con.close()
 
+    
 if __name__ ==  "__main__":
     app.run(debug=True)
-
-
-
-
-# <td><a href="{{url_for('delete_record', id=res['pid'])}}" class="btn btn-danger btn-block">Delete</a></td>
