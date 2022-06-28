20b80951432e28b01f4a906a771820d0353b28f6
Bruno Ferrari Senhora <brunoferrarisenhora@gmail.com>
2022-06-28 03:24:44 -0300

    Implementação do Back-end
    Embrião já funcional do back-end.

--- SNEMP/app.py
+++ SNEMP/app.py
@@ -1,27 +1,57 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 import pandas as pd
-import csv
 import os
 
 app = Flask(__name__, template_folder="templates")
+app.config['UPLOAD_FOLDER'] = "static/Excel"
+app.secret_key = "123"
+
+con=sqlite3.connect("MyData.db")
+con.execute("create table if not exists data(pid integer primary key, exceldata TEXT)")
+con.close()
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
+    if request.method == "POST":
+        csvfile = request.files['csvfile']
+        if csvfile.filename != '':
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], csvfile.filename)
+            csvfile.save(filepath)
+            con = sqlite3.connect("MyData.db")
+            cur = con.cursor()
+            cur.execute("insert into data(exceldata)values(?)", (csvfile.filename,))
+            con.commit()
+
+            con = sqlite3.connect("MyData.db")
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute("select * from data")
+            data = cur.fetchall()
+            con.close
+            return render_template("snemp.html", data=data)
+
     return render_template("snemp.html")
 
-@app.route("/data", methods=["GET", "POST"])
-def data():
-    if request.method == "POST":
-        f = request.form["csvfile"]
-        data = []
-        with open(f) as file:
-            csvfile = csv.reader(file)
-            for row in csvfile:
-                data.append(row)
-        data = pd.DataFrame(data)
-        return render_template("data.html", data = data)
-
+@app.route('/bd/<string:id>')
+def bd(id):
+    con = sqlite3.connect("MyData.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from data where pid=?",(id))
+    data = cur.fetchall()
+    for val in data:
+        path = os.path.join("static/Excel/",val[1])
+        data = pd.read_csv(path)
+        data.to_html("output.html")
+    con.close()
+    return render_template("bd.html", data=data.to_html(index=False, classes="table table-bordered text-white".replace('<th>', '<th style="text-align:center">')))
 
 
 if __name__ ==  "__main__":
     app.run(debug=True)
+
+
+
+
+# <td><a href="{{url_for('delete_record', id=res['pid'])}}" class="btn btn-danger btn-block">Delete</a></td>
